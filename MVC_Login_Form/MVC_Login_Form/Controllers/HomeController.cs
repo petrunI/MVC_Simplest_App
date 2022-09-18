2c5dbdfa847d61bc7368f0ff714c639644889826
petrunI <igorpetruni65@gmail.com>
2022-09-18 13:33:16 +0300

    Updated login form: added required functional, slightly changed ui; added login controller, added redirect in case of trying to get to the Check route by using URL

--- MVC_Login_Form/MVC_Login_Form/Controllers/HomeController.cs
+++ MVC_Login_Form/MVC_Login_Form/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace MVC_Login_Form.Controllers
@@ -12,14 +13,7 @@ namespace MVC_Login_Form.Controllers
     
     public class HomeController : Controller
     {
-        List<LoginModel> userList = new List<LoginModel>
-        {
-                new LoginModel{ Login ="vharbar", Password = "1234", Admin = true},
-                new LoginModel{ Login ="ipetrunin", Password = "4321", Admin = false},
-                new LoginModel{ Login ="konchilas", Password = "fantasia", Admin = true},
-                new LoginModel{ Login ="user1", Password = "pass1", Admin = false}
-        };
-
+       
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -29,15 +23,6 @@ namespace MVC_Login_Form.Controllers
 
         public IActionResult Index()
         {
-            return View();
-        }
-        public IActionResult Login()
-        {
-            return View();
-        }
-        public IActionResult Check(LoginModel model)
-        {
-           
             return View();
         }
 
---
+++ MVC_Login_Form/MVC_Login_Form/Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Login_Form.Models;
+using System.Collections.Generic;
+
+namespace MVC_Login_Form.Controllers
+{
+    public class LoginController : Controller
+    {
+        List<LoginModel> userList = new List<LoginModel>
+        {
+                new LoginModel{ Login ="vharbar", Password = "1234", Admin = true},
+                new LoginModel{ Login ="ipetrunin", Password = "4321", Admin = false},
+                new LoginModel{ Login ="konchilas", Password = "fantasia", Admin = true},
+                new LoginModel{ Login ="user1", Password = "pass1", Admin = false}
+        };
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Check(LoginModel model)
+        {
+            foreach (LoginModel user in userList)
+            {
+                if (model.Login == user.Login && model.Password == user.Password)
+                {
+                    return View(user);
+                }
+            }
+            if (model.Login == null && model.Password == null)
+                return Redirect("Login/");
+            return View();
+        }
+
+    }
+}
